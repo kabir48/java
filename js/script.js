cebe769947af5c37b2f93a8afdf1a4c501368b51
kabir48 <h.kabir1498@gmail.com>
2021-06-14 23:18:41 +0600

    two file added

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// var x=prompt("Enter Any Number");
+//x+=4;//x=x+4
+// console.log('the Number is:'+x);
+// var num1=prompt("Enter Any Number");
+// var num2=prompt("Enter Any Number two");
+// num1=parseInt(num1,10);
+// num2=parseInt(num2,10);
+// var result,sub,mul,div,rem;
+// result=num1+num2;
+// sub=num1-num2;
+// mul=num1*num2;
+// div=num1/num2;
+// rem=num1%num2;
+// document.write('The First Number:'+num1+"</br>"+'The Second Number:'+num2+"</br>"+'total:'+result+"</br>");
+// document.write('The First Number:'+num1+"</br>"+'The Second Number:'+num2+"</br>"+'total:'+sub+"</br>");
+// document.write('The First Number:'+num1+"</br>"+'The Second Number:'+num2+"</br>"+'total:'+mul+"</br>");
+// document.write('The First Number:'+num1+"</br>"+'The Second Number:'+num2+"</br>"+'total:'+div+"</br>");
+// document.write('The First Number:'+num1+"</br>"+'The Second Number:'+num2+"</br>"+'total:'+rem+"</br>");
+
+// var f=prompt("Enter Celcius Value");
+//  f=(f-32)*5/9;
+//  document.write('The Farhenhiet Value:'+f);
+
+// var num1=prompt("Enter Any Number");
+// var num2=prompt("Enter Any Number two");
+// if(num1>num2)
+// console.log(num1+'is a Big Number');
+// else
+// console.log(num2+'is a Big Number');
+
+// var year=prompt("Enter any Year ");
+// if(year%4==0)
+// console.log('leaf Year');
+// else 
+// console.log('Not leaf Year');
+// && logical opertator//and gate 2 tai true hote hobe like,11,00 
+// ||//or gate jekono akta true holei hobe like 01=1
+// 11=1
+// 01=1
+
+// var letter=prompt("Enter any Vowel");
+// if(letter=="a"||letter=="e"||letter=="i"||letter=="o"||letter=="u")
+// console.log('vowel');
+// else
+// console.log('consonent');
+var num1=prompt("Enter any number1");
+var num2=prompt("Enter any number2");
+var num3=prompt("Enter any number3");
+if(num1>num2 && num1>num3)
+document.write(num1+"&nbsp;"+'<h4>Number one is greater</h4>');
+else if(num2>num1 && num2>num3)
+document.write(num2+"&nbsp;"+'<h4>Number two is greater</h4>');
+else
+document.write(num3+"&nbsp;"+'<h4>Number three is greater</h4>');
+
+
+
+
+
+
+
+
+
+
